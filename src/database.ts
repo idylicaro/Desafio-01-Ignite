7d64caae04cb8af974afb58e13c1fb99d6a04de9
Idyl Icaro dos Santos <idylicaro.se@gmail.com>
2023-06-27 20:17:08 -0300

    feat: create database in memory using json

--- src/database.ts
+++ src/database.ts
@@ -1,5 +1,25 @@
+import fs from 'node:fs/promises';
+
+// The URL constructor is used to create a URL object from a string.
+// The PATH is relative to the current file (database.ts).
+const DATABASEPATH = new URL('../database.json', import.meta.url);
+
 export class Database {
   #database = {};
+
+  constructor() {
+    fs.readFile(DATABASEPATH, 'utf8')
+      .then((data) => {
+        this.#database = JSON.parse(data);
+      })
+      .catch(() => {
+        this.#persist();
+      });
+  }
+
+  #persist = async () => {
+    await fs.writeFile(DATABASEPATH, JSON.stringify(this.#database));
+  };
 
   select(table) {
     const data = this.#database[table] ?? [];
