1f0a7e5088fcd9c2e844a7ffbb05df37abc363db
Idyl Icaro dos Santos <idylicaro.se@gmail.com>
2023-06-27 23:01:08 -0300

    refactor: improve types

--- src/routes.ts
+++ src/routes.ts
@@ -1,24 +1,37 @@
 import { randomUUID } from 'node:crypto';
 import { Database } from './database.js';
 
+export interface Task {
+  id: string;
+  title: string;
+  description: string;
+}
+
 const database = new Database();
 
-export const routes = [
+export interface Route {
+  path: string;
+  method: string;
+  handler: (req: any, res: any) => Promise<void>;
+}
+
+export const routes: Route[] = [
   {
     path: '/tasks',
     method: 'GET',
-    handler: async (req: any, res: any) => {
-      const tasks = database.select('tasks');
+    handler: async (req: any, res: any): Promise<void> => {
+      const tasks: Task[] = database.select('tasks');
       res.end(JSON.stringify(tasks));
     }
   },
   {
     path: '/tasks',
     method: 'POST',
-    handler: async (req: any, res: any) => {
-      const { title, description } = req.body;
+    handler: async (req: any, res: any): Promise<void> => {
+      const { title, description }: { title: string; description: string } =
+        req.body;
 
-      const task = {
+      const task: Task = {
         id: randomUUID(),
         title,
         description
--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,23 @@
 import http from 'node:http';
-import { routes } from './routes.js';
+import { routes, Route } from './routes.js';
 import { json } from './middlewares/json.js';
 
-const server = http.createServer(async (req, res) => {
-  const { method, url } = req;
+const server = http.createServer(
+  async (req: http.IncomingMessage, res: http.ServerResponse) => {
+    const { method, url } = req;
 
-  await json(req, res);
+    await json(req, res);
 
-  const route = routes.find((route) => {
-    return route.method === method && route.path === url;
-  });
+    const route: Route | undefined = routes.find((route: Route) => {
+      return route.method === method && route.path === url;
+    });
 
-  if (route) {
-    return route.handler(req, res);
+    if (route) {
+      return route.handler(req, res);
+    }
+    return res.writeHead(404).end();
   }
-  return res.writeHead(404).end();
-});
+);
 
 server.listen(3333);
 
