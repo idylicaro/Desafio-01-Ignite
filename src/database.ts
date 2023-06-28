df8728a3b231397b7ac2b0f29c212828936a4308
Idyl Icaro dos Santos <idylicaro.se@gmail.com>
2023-06-27 21:01:25 -0300

    refactor: improve types

--- src/database.ts
+++ src/database.ts
@@ -1,15 +1,19 @@
 import fs from 'node:fs/promises';
 
+interface DatabaseTable {
+  [key: string]: any[];
+}
+
 // The URL constructor is used to create a URL object from a string.
 // The PATH is relative to the current file (database.ts).
 const DATABASEPATH = new URL('../database.json', import.meta.url);
 
 export class Database {
-  #database = {};
+  #database: DatabaseTable = {};
 
   constructor() {
     fs.readFile(DATABASEPATH, 'utf8')
-      .then((data) => {
+      .then((data: string) => {
         this.#database = JSON.parse(data);
       })
       .catch(() => {
@@ -21,12 +25,12 @@ export class Database {
     await fs.writeFile(DATABASEPATH, JSON.stringify(this.#database));
   };
 
-  select(table) {
+  select(table: string): any[] {
     const data = this.#database[table] ?? [];
     return data;
   }
 
-  insert(table, data) {
+  insert(table: string, data: any): any {
     if (Array.isArray(this.#database[table])) {
       this.#database[table].push(data);
     } else {
